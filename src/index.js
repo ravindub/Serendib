75a17c8e6b1b2ff79448d8b6fd45d3b185ea3067
RuwanPeiris <ruwanpeiris1991@gmail.com>
2019-04-10 18:02:51 +0530

    create simple component EventCard Readmore and Post component

--- src/index.js
+++ src/index.js
@@ -5,8 +5,11 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import Home from './components/Home/Home';
 import Header from './components/Header/Header';
+import Post from './components/Post/Post';
+import EventCard from './components/EventCard/EventCard';
 
-ReactDOM.render(<Header />, document.getElementById('root'));
+ReactDOM.render(<EventCard />, document.getElementById('root'));
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
