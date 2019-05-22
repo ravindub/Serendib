5659d72c320bf44309476a4f5f8d52c622723ecc
RuwanPeiris <ruwanpeiris1991@gmail.com>
2019-05-22 23:43:42 +0530

    Event card simple change and Blog.js to add Event card

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -4,6 +4,7 @@ import old_pic from "./old.png";
 import share from './share-512.png';
 import add_button from './Button-512.png';
 import './Blog.css';
+import EventCard from '../EventCard/EventCard';
 
 class Blog extends Component {
 
@@ -19,25 +20,29 @@ class Blog extends Component {
             <div id="mainDiv">
                 <div id="leftDiv">
                     <div id="list1">
-                    <a id="mostReasonLi" href="">
-                        <img className="recent_icon" src={recent_pic} alt="icon"/> Most Recent
-                    </a><br/>
+                        <a id="mostReasonLi" href="">
+                            <img className="recent_icon" src={recent_pic} alt="icon"/> Most Recent
+                        </a><br/>
                     </div>
                     <div id="list2">
-                    <a id="mostReasonLi" href="">
-                        <img className="old_icon" src={old_pic}  alt="icon"/> Old Posts
-                    </a><br/>
+                        <a id="mostReasonLi" href="">
+                            <img className="old_icon" src={old_pic} alt="icon"/> Old Posts
+                        </a><br/>
                     </div>
                 </div>
                 <div id="centerDiv">
-                    <form>
-                    <textarea rows="12" cols="100"/><br/>
-                    <a><img className="old_icon" src={share}  alt="icon"/> </a>
-                    <a><img className="old_icon" src={add_button}  alt="icon"/> </a>
-                    <input type="file" name="pic" accept="image/*" size="30"/>
-                    </form>
-
-
+                    <div id="center_form_div">
+                        <form>
+                            <textarea rows="12" cols="120"/><br/>
+                            <a><img className="old_icon" src={share} alt="icon"/> </a>
+                            <a><img className="old_icon" src={add_button} alt="icon"/> </a>
+                            <input type="file" name="pic" accept="image/*" size="30"/>
+                        </form>
+                    </div>
+                    <hr/>
+                    <div id="center_content_div">
+                        <EventCard/>
+                    </div>
                 </div>
                 <div id="rightDiv"></div>
                 <div>
